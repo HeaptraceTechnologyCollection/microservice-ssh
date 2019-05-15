01ca10e72c923082e04d312ee4ec5a48737eb29e
ht68rohit <rohits@heaptrace.com>
2019-05-15 16:52:10 +0530

    ssh public/private key support

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	result "github.com/heaptracetechnology/microservice-ssh/result"
 	"golang.org/x/crypto/ssh"
@@ -14,13 +15,18 @@ type SSHArguments struct {
 	Password string `json:"password,omitempty"`
 }
 
-var pwd string
+type Message struct {
+	Success    string `json:"success"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"statuscode"`
+}
 
 //SSH
 func SSH(responseWriter http.ResponseWriter, request *http.Request) {
 
 	var host = os.Getenv("HOST")
 	var port = os.Getenv("PORT")
+	var privateKey = os.Getenv("PRIVATE_KEY")
 
 	decoder := json.NewDecoder(request.Body)
 
@@ -31,7 +37,13 @@ func SSH(responseWriter http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	pwd = param.Password
+	if param.Password != "" && privateKey != "" {
+		message := Message{"false", "Please provide either password or private key", http.StatusBadRequest}
+		bytes, _ := json.Marshal(message)
+		result.WriteJsonResponse(responseWriter, bytes, http.StatusBadRequest)
+		return
+	}
+
 	var hostname string
 	if port != "" {
 		hostname = host + ":" + port
@@ -39,13 +51,15 @@ func SSH(responseWriter http.ResponseWriter, request *http.Request) {
 		hostname = host + ":22"
 	}
 
-	client, session, err := connectToHost(param.Username, hostname)
+	client, session, err := connectToHost(param.Username, hostname, param.Password, privateKey)
 	if err != nil {
-		panic(err)
+		result.WriteErrorResponse(responseWriter, err)
+		return
 	}
-	out, err := session.CombinedOutput(param.Command)
-	if err != nil {
-		panic(err)
+	out, outputErr := session.CombinedOutput(param.Command)
+	if outputErr != nil {
+		result.WriteErrorResponse(responseWriter, outputErr)
+		return
 	}
 
 	bytes, _ := json.Marshal(string(out))
@@ -54,11 +68,33 @@ func SSH(responseWriter http.ResponseWriter, request *http.Request) {
 
 }
 
-func connectToHost(user, host string) (*ssh.Client, *ssh.Session, error) {
+func connectToHost(user, host, password, privateKey string) (*ssh.Client, *ssh.Session, error) {
 
-	sshConfig := &ssh.ClientConfig{
-		User: user,
-		Auth: []ssh.AuthMethod{ssh.Password(pwd)},
+	var sshConfig *ssh.ClientConfig
+
+	if password != "" {
+		config := &ssh.ClientConfig{
+			User: user,
+			Auth: []ssh.AuthMethod{ssh.Password(password)},
+		}
+		sshConfig = config
+
+	} else if privateKey != "" {
+		pemBytes, err := base64.StdEncoding.DecodeString(privateKey)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		signer, err := ssh.ParsePrivateKey(pemBytes)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		config := &ssh.ClientConfig{
+			User: user,
+			Auth: []ssh.AuthMethod{ssh.PublicKeys(signer)},
+		}
+		sshConfig = config
 	}
 
 	sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
